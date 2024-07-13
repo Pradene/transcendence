0b154fdd2e38fe4c2cff6cbd917ba4e5fbeea6a8
Pierre DUCLOUX <pducloux@made-f0Br6s7.clusters.42paris.fr>
2024-07-13 17:16:23 +0200

    update

--- back/static/app/js/views/Home.js
+++ back/static/app/js/views/Home.js
@@ -14,6 +14,18 @@ export class Home extends AbstractView {
         return `
         <nav-component></nav-component>
         <h1>Hello World</h1>
+        <div class="game-container"> <!-- Game Container, contain the game and the list of available rooms -->
+		    <div class="game-container-header">
+		    	<button class="create-game">Create Game</button> <!-- Create Game Button -->
+		    	<button class="create-tournament">Create Tournament</button> <!-- Create Tournament Button -->
+		    	<button class="refresh-room">Refresh Room</button> <!-- Refresh Room Button -->
+		    </div>
+		    <div class="game"></div> <!-- Game Container, contain the game -->
+		    <div class="available-games"> <!-- Available Games Container, contain the list of available games -->
+		    	<div class="available-game"></div> <!-- Available Game Container, contain the list of available games -->
+		    	<div class="available-tournament"></div> <!-- Available Tournament Container, contain the list of available tournaments -->
+		    </div>
+	    </div>
         <script src="/static/scrips/pong/dist/main.js" type="application/javascript"></script>
         `
     }
