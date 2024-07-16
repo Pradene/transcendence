e439820e62ee49b4cf33a4f97715467e7093f319
Pierre DUCLOUX <pducloux@made-f0Ar4s6.clusters.42paris.fr>
2024-07-16 16:04:05 +0200

    update

--- web/static/app/js/views/Home.js
+++ web/static/app/js/views/Home.js
@@ -1,24 +1,43 @@
 import { AbstractView } from "./AbstractView.js"
 
-let isGameloaded = false
+let game_loaded = false
 
 export class Home extends AbstractView {
     constructor() {
         super()
     }
 
-    getHtml() {
-        if (!isGameloaded) {
-            const head = document.getElementsByTagName('head')[0]
-            const script = document.createElement('script')
-            script.src = '/static/scripts/pong/dist/main.js'
-            head.appendChild(script)
-            isGameloaded = true
-        }
+    async getHtml() {
+        const head = document.querySelector('head')
 
         return `
         <nav-component></nav-component>
         <h1>Hello World</h1>
+        <div class="game-container"> <!-- Game Container, contain the game and the list of available rooms -->
+		    <div class="game-container-header">
+		    	<button class="create-game">Create Game</button> <!-- Create Game Button -->
+		    	<button class="create-tournament">Create Tournament</button> <!-- Create Tournament Button -->
+		    	<button class="refresh-room">Refresh Room</button> <!-- Refresh Room Button -->
+		    </div>
+		    <div class="game"></div> <!-- Game Container, contain the game -->
+		    <div class="available-games"> <!-- Available Games Container, contain the list of available games -->
+		    	<div class="available-game"></div> <!-- Available Game Container, contain the list of available games -->
+		    	<div class="available-tournament"></div> <!-- Available Tournament Container, contain the list of available tournaments -->
+		    </div>
+	    </div>
         `
+    }
+
+    async render(container) {
+        container.innerHTML = await this.getHtml()
+        this.addEventListeners()
+
+        if (!game_loaded) {
+            const script = document.createElement('script')
+            const appcontainer = document.querySelector('div#app')
+            script.src = 'static/scripts/pong/dist/main.js'
+            appcontainer.appendChild(script)
+            // game_loaded = true
+        }
     }
 }
